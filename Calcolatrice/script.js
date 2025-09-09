6e4e0765fc82e31fb792972d34dcd3e15e3e1909
Your Name <martuccisilvia.dev@gmail.com>
2025-09-09 14:54:29 +0200

    Aggiunto progetto Calcolatrice dentro FrontEndEsercizi

---
+++ Calcolatrice/script.js
@@ -0,0 +1,7 @@
+function calcola() {
+  let n1 = Number(document.getElementById("n1").value);
+  let n2 = Number(document.getElementById("n2").value);
+
+  let somma = n1 + n2;
+  document.getElementById("risultato").textContent = "Risultato: " + somma;
+}
